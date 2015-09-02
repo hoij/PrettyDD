a5ad67ac7e9ed9b6a4852b1b05968c008366d6ec
hoij <marcus.hoij@gmail.com>
2015-09-02 16:50:34 +0200

    Fix indentation.

--- source/main.cpp
+++ source/main.cpp
@@ -22,7 +22,7 @@
 std::string currentTime() {
     std::chrono::time_point<std::chrono::system_clock> time_p = std::chrono::system_clock::now();
     std::time_t time = std::chrono::system_clock::to_time_t(time_p);  // Time since 1970
-    return std::ctime(&time);  // ctime converts to string of todays date
+    return std::ctime(&time);  // ctime converts to readable string
 }
 
 int main(void) {
@@ -37,32 +37,32 @@ int main(void) {
     Parser parser(config.getplayerRunningProgram());
     PlayerVector<Player*> pv;
 
-	std::ifstream logstream("../../test/example_lines.txt");
+    std::ifstream logstream("../../test/example_lines.txt");
 
-	if (logstream.is_open()) {
-		// Go to the end of the file
-		//logstream.seekg(0, logstream.end);
-		//logstream.close();
+    if (logstream.is_open()) {
+        // Go to the end of the file
+        //logstream.seekg(0, logstream.end);
+        //logstream.close();
 
-		// Copying a log file with all examples for testing purposes
-		//ifstream inlogstream("test_damage.txt");
-		//ofstream outlogstream("log.txt");
-		//while (getline(inlogstream, line)) {
-		//	outlogstream << line << endl;
-		//}
-		//outlogstream.close();
-		//inlogstream.close();
+        // Copying a log file with all examples for testing purposes
+        //ifstream inlogstream("test_damage.txt");
+        //ofstream outlogstream("log.txt");
+        //while (getline(inlogstream, line)) {
+        //	outlogstream << line << endl;
+        //}
+        //outlogstream.close();
+        //inlogstream.close();
 
-		//ifstream logstream("log.txt");
+        //ifstream logstream("log.txt");
 
         std::string logLine;
-		bool is_running = true;
-		while (is_running) {
-			while (getline(logstream, logLine)) {
-				std::cout << logLine << std::endl;
-				FormattedLine formattedLine(logLine);
+        bool is_running = true;
+        while (is_running) {
+            while (getline(logstream, logLine)) {
+                std::cout << logLine << std::endl;
+                FormattedLine formattedLine(logLine);
                 // TODO: Figure out some way to handling this check nicer.
-				if (formattedLine.isFormatted()) {
+                if (formattedLine.isFormatted()) {
                     LineInfo lineInfo = parser.parse(formattedLine);
                     if(lineInfo.hasStats) {
                         pv.addToPlayers(lineInfo);
@@ -77,22 +77,22 @@ int main(void) {
                     }
                 }
             }
-			if (!logstream.eof()) {  // Why did I check for this?
-				std::cout << "Error: Not EOF!" << std::endl;
-				is_running = false;
-			}
-			logstream.clear();
-			std::this_thread::sleep_for(std::chrono::milliseconds(250));
-		}
-	}
-	else {
-		std::cerr << "Could not open the log file." << std::endl;
-	}
+            if (!logstream.eof()) {  // Why did I check for this?
+                std::cout << "Error: Not EOF!" << std::endl;
+                is_running = false;
+            }
+            logstream.clear();
+            std::this_thread::sleep_for(std::chrono::milliseconds(250));
+        }
+    }
+    else {
+        std::cerr << "Could not open the log file." << std::endl;
+    }
 
     errorLog.write("");
-	errorLog.write("Program ended at: ", false);
+    errorLog.write("Program ended at: ", false);
     errorLog.write(currentTime());
 
-	std::getchar();
-	return 0;
+    std::getchar();
+    return 0;
 }
